4c80599f75e5953b9edf307bffa66eab76a94db0
GCdM <59828466+GCdM@users.noreply.github.com>
2024-10-20 02:06:46 +0100

    Add notes for future scripts in main script

---
+++ code/gcdemv/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Setup snapper + btrfs-grub
+
+# Terminal + shell + basic utils
+# - alacritty
+# - ttf-jetbrains-mono-nerd
+# - fish
+# - curl
+# - tar
+
+# Config github (+ SSH)
+
+# Hyprland utilities
+# - hyprpaper
+# - pipewire
+# - brightnessctl
+# - polkit?
+
+# IDE + dev tools
+# - npm
+# - clang
+# - ripgrep
+# - git
+# - neovim
+# - lazygit
+# - fd
+
+# Notification system
+# - dunst
+# - -- styling
+
+# Launcher
+# - wofi
+# - -- styling
+
+# Betterfox
+#
+#
