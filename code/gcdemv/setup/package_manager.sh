b3044de47f4e5bb309c6e2a31de21d957420968e
GCdM <59828466+GCdM@users.noreply.github.com>
2024-10-20 02:00:30 +0100

    Add package_manager script for installing paru

---
+++ code/gcdemv/setup/package_manager.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Use paru unless $PACKAGE_MANAGER is set
+package_manager="${PACKAGE_MANAGER:-paru}"
+
+# Check if the package_manager is already installed
+if command -v "$package_manager" &>/dev/null; then
+  echo "⏭️ $package_manager is already installed."
+else
+  echo ":dottedface: $package_manager is not installed."
+
+  read -p ":questionmark: Do you want to install it? (y/n): " answer
+
+  case $answer in
+  [Yy]*)
+    echo ":sandtimer: Installing $package_manager..."
+
+    sudo $package_manager -S --needed base-devel
+    git clone https://aur.archlinux.org/paru.git
+    cd paru
+    makepkg -si
+    ;;
+  *)
+    echo ":cross: Exiting without installing."
+    ;;
+  esac
+fi
