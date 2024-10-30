a3d72aa69bd593fc060442caac5ccaf4cd78cf85
GCdM <59828466+GCdM@users.noreply.github.com>
2024-10-30 21:28:36 +0000

    Add notes on snapper todo

---
+++ install/snapper.sh
@@ -0,0 +1,6 @@
+# - Understand different subvolume configurations
+#   - Can I keep a home directory whilst reinstalling Linux?
+# - Document chosen "partitioning"
+# - Understand fstab
+#   - Choose a configuration for this laptop
+# - install snapper-tools?? and grub-btfrs
