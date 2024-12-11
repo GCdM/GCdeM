ad00e481add8ee65259ef640b2045225b9d9efde
GCdM <59828466+GCdM@users.noreply.github.com>
2024-12-11 13:58:59 +0000

    Add GCdeM ASCII art to parent install script

--- install.sh
+++ install.sh
@@ -2,7 +2,22 @@
 
 set -e
 
-echo "Running GCdeM installation script"
+echo '=== ======================================================================= ==='
+echo '=== ======================================================================= ==='
+echo '===       ___           ___          _____          ___           ___       ==='
+echo '===      /  /\         /  /\        /  /::\        /  /\         /__/\      ==='
+echo '===     /  /:/_       /  /:/       /  /:/\:\      /  /:/_       |  |::\     ==='
+echo '===    /  /:/ /\     /  /:/       /  /:/  \:\    /  /:/ /\      |  |:|:\    ==='
+echo '===   /  /:/_/::\   /  /:/  ___  /__/:/ \__\:|  /  /:/ /:/_   __|__|:|\:\   ==='
+echo '===  /__/:/__\/\:\ /__/:/  /  /\ \  \:\ /  /:/ /__/:/ /:/ /\ /__/::::| \:\  ==='
+echo '===  \  \:\ /~~/:/ \  \:\ /  /:/  \  \:\  /:/  \  \:\/:/ /:/ \  \:\~~\__\/  ==='
+echo '===   \  \:\  /:/   \  \:\  /:/    \  \:\/:/    \  \::/ /:/   \  \:\        ==='
+echo '===    \  \:\/:/     \  \:\/:/      \  \::/      \  \:\/:/     \  \:\       ==='
+echo '===     \  \::/       \  \::/        \__\/        \  \::/       \  \:\      ==='
+echo '===      \__\/         \__\/                       \__\/         \__\/      ==='
+echo '===                                                                         ==='
+echo '=== ======================================================================= ==='
+echo '=== ======================================================================= ==='
 
 # Set environment variables to be used across install sub-scripts
 export GCDEM_PATH="$HOME/GCdeM/"
@@ -14,22 +29,24 @@ export TERMINAL="alacritty"
 # pacman -Syu --noconfirm
 # pacman -S --noconfirm base-devel git wget curl unzip
 
+current_directory="$(dirname "$(realpath "$0")")"
+
 # Install package manager
-./install/00_package_manager.sh
+"$current_directory/install/00_package-manager.sh"
 
 # Set up terminal & shell
-./install/01_git.sh
+"$current_directory/install/01_git.sh"
 
 # Set up Hyprland
-./install/02_hyprland.sh
+"$current_directory/install/02_hyprland.sh"
 
 # TODO copy all `stdout` + `stderr` to a .gitignored file or directory, so that
 # it's auditable in case the terminal is cleared/closed or the computer rebooted
 
-read -pr "Would you like to reboot system? (y/N): " REBOOT
-if [[ $REBOOT =~ ^[Yy]$ ]]; then
-	reboot
-fi
+# read -pr "Would you like to reboot system? (y/N): " REBOOT
+# if [[ $REBOOT =~ ^[Yy]$ ]]; then
+# 	reboot
+# fi
 
 # # Set environment variables to be used across install sub-scripts
 # fish -c "set -Ux GCDEM_PATH $HOME/GCdeM"
