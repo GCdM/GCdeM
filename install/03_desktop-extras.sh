b0c58c42084794f8e4e4279b60d90af04f3fb433
GCdM <59828466+GCdM@users.noreply.github.com>
2025-01-17 09:37:35 +0000

    Add notes for pipewire GUIs

--- install/03_desktop-extras.sh
+++ install/03_desktop-extras.sh
@@ -2,6 +2,9 @@
 # - fd
 # - dunst
 # - pipewire
+#   - pwvucontrol - GNOME GUI for PipeWire
+#   _OR_
+#   - alsa-utils ? https://wiki.archlinux.org/title/Advanced_Linux_Sound_Architecture#ALSA_utilities
 # - nautilus
 # - wofi
 # - a GUI network manager
